91f3c84c23c459a5f83512c19167721ab9a6b881
Jaemoon <jaemoon@jaemoonpark.com>
2016-07-14 18:14:00 -0400

    add segue between two viewcontroller (strictly between the two)

--- Dice Incomplete/RollViewController.swift
+++ Dice Incomplete/RollViewController.swift
@@ -21,12 +21,7 @@ class RollViewController: UIViewController {
     }
 
     @IBAction func rollTheDice() {
-        var controller: DiceViewController
-        controller = self.storyboard?.instantiateViewControllerWithIdentifier("DiceViewController") as! DiceViewController
-        
-        controller.firstValue = self.randomDiceValue()
-        controller.secondValue = self.randomDiceValue()
-        self.presentViewController(controller, animated: true, completion: nil)
+        performSegueWithIdentifier("rollDice", sender: self)
     }
     
     
